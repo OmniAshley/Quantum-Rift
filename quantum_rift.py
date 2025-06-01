5bf6d8f7f8a0e7e222556a1dc414a0ace2f9df9c
Ashley Beebakee <n0970639@my.ntu.ac.uk>
2025-06-01 15:51:14 +0100

    chore: initial commit

---
+++ quantum_rift.py
@@ -0,0 +1,11 @@
+#----------------------------------------------------------#
+# Name: Quantum Rift
+# Purpose: A 2D indie game designed to keep a player mesme-
+#          rised through its creative aesthetics and features.
+# Author: Ashley Beebakee (https://github.com/OmniAshley)
+# Date Created: 01/06/2025
+# Last Updated: 01/06/2025
+# Python Version: 3.10.6
+#----------------------------------------------------------#
+
+print("Hello Quantum Rift!")
